119a92f3b81153d2cfec51e882ae39b3c9621451
Tsholofelo <hopendawonde@gmail.com>
2026-05-14 16:36:44 +0200

    Add ValidationError and ValueObject to SharedKernel
    Introduce ValidationError for aggregating validation errors and
    ValueObject as a DDD base class with value-based equality logic.

---
+++ SharedKernel/ValueObject.cs
@@ -0,0 +1,76 @@
+﻿namespace SharedKernel;
+
+/// <summary>
+/// Base class for value objects following DDD principles.
+/// Value objects are immutable and compared by their value, not identity.
+/// </summary>
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    /// <summary>
+    /// Gets the atomic values that compose this value object.
+    /// </summary>
+    /// <returns>An enumerable of atomic values.</returns>
+    protected abstract IEnumerable<object> GetAtomicValues();
+
+    /// <summary>
+    /// Determines whether the specified value object is equal to the current value object.
+    /// </summary>
+    /// <param name="other">The value object to compare with the current value object.</param>
+    /// <returns>true if the specified value object is equal to the current value object; otherwise, false.</returns>
+    public bool Equals(ValueObject? other)
+    {
+        return other is not null && ValuesAreEqual(other);
+    }
+
+    /// <summary>
+    /// Determines whether the specified object is equal to the current value object.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current value object.</param>
+    /// <returns>true if the specified object is equal to the current value object; otherwise, false.</returns>
+    public override bool Equals(object? obj)
+    {
+        return obj is ValueObject other && Equals(other);
+    }
+
+    /// <summary>
+    /// Returns the hash code for this value object.
+    /// </summary>
+    /// <returns>A hash code for the current value object.</returns>
+    public override int GetHashCode()
+    {
+        return GetAtomicValues()
+            .Aggregate(1, (current, obj) => HashCode.Combine(current, obj));
+    }
+
+    /// <summary>
+    /// Determines whether two value objects are equal.
+    /// </summary>
+    /// <param name="left">The first value object to compare.</param>
+    /// <param name="right">The second value object to compare.</param>
+    /// <returns>true if the value objects are equal; otherwise, false.</returns>
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        return left?.Equals(right) ?? right is null;
+    }
+
+    /// <summary>
+    /// Determines whether two value objects are not equal.
+    /// </summary>
+    /// <param name="left">The first value object to compare.</param>
+    /// <param name="right">The second value object to compare.</param>
+    /// <returns>true if the value objects are not equal; otherwise, false.</returns>
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+    {
+        return !(left == right);
+    }
+
+    /// <summary>
+    /// Checks if the atomic values of two value objects are equal.
+    /// </summary>
+    /// <param name="other">The other value object to compare.</param>
+    /// <returns>true if all atomic values are equal; otherwise, false.</returns>
+    private bool ValuesAreEqual(ValueObject other)
+    {
+        return GetAtomicValues().SequenceEqual(other.GetAtomicValues());
+    }
+}
