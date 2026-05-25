9f840ea7cb3e4c177593cd35ab73025087e1a828
Tsholofelo <hopendawonde@gmail.com>
2026-05-25 11:08:52 +0200

    Add messaging abstractions and SharedKernel reference
    Added a `ProjectReference` to `SharedKernel.csproj` in the
    `clean-architecture.application.csproj` file. Introduced
    messaging abstractions in the `clean_architecture.application.Abstractions.Messaging`
    namespace, including `ICommand`, `ICommandHandler`, `IQuery`,
    and `IQueryHandler` interfaces. These abstractions utilize the
    `Result` type from the SharedKernel project to standardize
    command and query handling.

---
+++ clean-architecture.application/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using SharedKernel;
+
+namespace clean_architecture.application.Abstractions.Messaging;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{
+    Task<Result<TResponse>> Handle(TQuery query, CancellationToken cancellationToken);
+}
