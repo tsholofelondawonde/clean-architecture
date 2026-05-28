429e2212006dd10f9544193a96768d059ba1f198
Tsholofelo <hopendawonde@gmail.com>
2026-05-28 09:59:42 +0200

    Add EF Core and ApplicationDbContext implementation
    - Added `Microsoft.EntityFrameworkCore` and `Microsoft.EntityFrameworkCore.Relational` package references.
    - Added a project reference to `clean-architecture.application`.
    - Implemented `ApplicationDbContext` with `Notes` DbSet and domain event publishing.
    - Configured default schema as `dbo` using the new `Schemas` class.

---
+++ clean-architecture.infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,67 @@
+﻿using clean_architecture.application.Abstractions.Data;
+using clean_architecture.domain.NotesManagement.Aggregates;
+using clean_architecture.infrastructure.DomainEvents;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace clean_architecture.infrastructure.Database;
+
+public class ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options,
+    IDomainEventsDispatcher? domainEventsDispatcher) : DbContext(options), IApplicationDbContext
+{
+    private readonly IDomainEventsDispatcher? _domainEventsDispatcher = domainEventsDispatcher;
+    public DbSet<Note> Notes { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        // When should you publish domain events?
+        //
+        // 1. BEFORE calling SaveChangesAsync
+        //     - domain events are part of the same transaction
+        //     - immediate consistency
+        // 2. AFTER calling SaveChangesAsync
+        //     - domain events are a separate transaction
+        //     - eventual consistency
+        //     - handlers can fail
+
+        int result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+
+        return result;
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder); // Ensure the base class configurations are applied
+
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        builder.HasDefaultSchema(Schemas.Default);
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        if (_domainEventsDispatcher is null)
+        {
+            return;
+        }
+
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                List<IDomainEvent> domainEvents = entity.DomainEvents;
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        await _domainEventsDispatcher.DispatchAsync(domainEvents);
+    }
+}
+
---
+++ clean-architecture.infrastructure/Database/Schemas.cs
@@ -0,0 +1,6 @@
+﻿namespace clean_architecture.infrastructure.Database;
+
+internal static class Schemas
+{
+    public const string Default = "dbo";
+}
