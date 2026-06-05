9b1905226e07a72ac3ab5b9f435768a9cacddb61
Tsholofelo <hopendawonde@gmail.com>
2026-06-05 12:10:24 +0200

    Add middleware and migration extension methods
    Added MiddlewareExtensions for request context logging and MigrationExtensions for applying EF Core migrations at startup. Also referenced infrastructure project in WebApi for shared services and types.

---
+++ clean-architecture.WebApi/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,23 @@
+﻿using clean_architecture.WebApi.Middleware;
+
+namespace clean_architecture.WebApi.Extensions;
+
+
+/// <summary>
+/// Provides extension methods for registering custom middleware in the application pipeline.
+/// </summary>
+public static class MiddlewareExtensions
+{
+    /// <summary>
+    /// Adds the <see cref="RequestContextLoggingMiddleware"/> to the application's request pipeline.
+    /// This middleware enriches Serilog logs with a correlation ID for each request.
+    /// </summary>
+    /// <param name="app">The <see cref="IApplicationBuilder"/> to add the middleware to.</param>
+    /// <returns>The <see cref="IApplicationBuilder"/> for chaining.</returns>
+    public static IApplicationBuilder UseRequestContextLogging(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<RequestContextLoggingMiddleware>();
+
+        return app;
+    }
+}
