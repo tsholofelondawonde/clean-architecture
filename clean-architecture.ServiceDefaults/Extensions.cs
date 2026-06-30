875ab9ff64266667d66ae5e0a2d4a0c796e238b1
Tsholofelo <hopendawonde@gmail.com>
2026-06-30 11:17:21 +0200

    Remove Aspire.Hosting.Orchestration.win-x64 package
    Removed unused Aspire.Hosting.Orchestration.win-x64 package reference. Refactored AddOpenTelemetryExporters to return void instead of TBuilder and updated its usage.

--- clean-architecture.ServiceDefaults/Extensions.cs
+++ clean-architecture.ServiceDefaults/Extensions.cs
@@ -78,7 +78,7 @@ public static class Extensions
         return builder;
     }
 
-    private static TBuilder AddOpenTelemetryExporters<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
+    private static void AddOpenTelemetryExporters<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
     {
         var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
 
@@ -93,8 +93,6 @@ public static class Extensions
         //    builder.Services.AddOpenTelemetry()
         //       .UseAzureMonitor();
         //}
-
-        return builder;
     }
 
     public static TBuilder AddDefaultHealthChecks<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
